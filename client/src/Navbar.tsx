1fbc06ba74d78851d8103767a20dee8fa2c0a632
Hilari Gebremedhin <hilarygebremedhn28@gmail.com>
2026-03-01 03:21:16 -0800

    Update project files

--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         <div className="flex items-center gap-3">
           <div className="h-12 w-12 overflow-visible -my-2 flex items-center">
             <img
-              src="/kushna%20logo.jpeg"
+              src="/kushina%20logo.png"
               alt="Kushena logo"
               className="h-16 w-auto object-contain"
             />
